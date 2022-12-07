98f2bb0dfcd53fa12e966920bbd7ec6edc7dba10
Udayan Misra <83898487+UdayanMisra2000@users.noreply.github.com>
2022-12-07 22:26:45 +0530

    Create Slides.js

---
+++ src/Components/Main/Slides/Slides.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Slides.css'
+import img10 from './slide1.jpg'
+
+export default function Slides() {
+    return (
+        <div>
+           <img className="slide_img" src={img10}/> 
+        </div>
+    )
+}
